f8e21af2ba3f851b94777a047174bf7da5292af3
aklimk <andrewklimko@outlook.com>
2024-11-26 01:01:07 +1000

    Changed projected structure. Seperated network from example.
    Changed the project structure to seperate the network library
    from the example MNIST classification program. Merged the network
    and utilities programs and moved NetworkData to the network library.
    Changed build process to not include source libs by default and
    allow user to specify their path.

---
+++ example/main.cpp
@@ -0,0 +1,30 @@
+#include "loadimages.hpp"
+#include "network.hpp"
+
+int main() {
+    // Create a neural network with appropriate structure.
+    // Input imagse are 784 float values, output is a single-class 
+    // classification of a handwritten digit from 0-9.
+    NeuralNetwork network = NeuralNetwork({784, 64, 32, 10});
+
+    // Load training and testing data.
+    std::string project_dir = R"(C:/users/andre/Documents/neuralnet/mnist)";
+    NetworkData training_data = LoadMnistDataPair(
+        project_dir + "/train-images.idx3-ubyte",
+        project_dir + "/train-labels.idx1-ubyte"
+    );
+    NetworkData testing_data = LoadMnistDataPair(
+        project_dir + "/t10k-images.idx3-ubyte",
+        project_dir + "/t10k-labels.idx1-ubyte"
+    );
+
+    // Perform stochastic gradient descent on the network.
+    StochasticGradientDescent(
+        network,
+        training_data,
+        testing_data,
+        5, // Epochs.
+        0.05, // Learning rate.
+        20 // Batch size.
+    );
+}
