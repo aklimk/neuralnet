a052f195be031c455e68a73c5b43d19992bc7837
aklimk <andrewklimko@outlook.com>
2024-11-26 01:13:18 +1000

    Added path cli parameter.
    Path to MNIST data folder is specified as a command line parameter
    instead of hardcoded.

--- example/main.cpp
+++ example/main.cpp
@@ -1,14 +1,22 @@
+#include <iostream>
+
 #include "loadimages.hpp"
 #include "network.hpp"
 
-int main() {
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cout << "Path to MNIST data not specified. Please specify path as second argument." << std::endl;
+        return 1;
+    }
+    
+    std::string project_dir(argv[1]);
+    
     // Create a neural network with appropriate structure.
     // Input imagse are 784 float values, output is a single-class 
     // classification of a handwritten digit from 0-9.
     NeuralNetwork network = NeuralNetwork({784, 64, 32, 10});
 
     // Load training and testing data.
-    std::string project_dir = R"(C:/users/andre/Documents/neuralnet/mnist)";
     NetworkData training_data = LoadMnistDataPair(
         project_dir + "/train-images.idx3-ubyte",
         project_dir + "/train-labels.idx1-ubyte"
